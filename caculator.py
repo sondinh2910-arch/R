1a8bf001f325ff2afb9505af99c70174723ccb3f
sondinh2910-arch <sondinh2910@gmail.com>
2026-06-12 19:16:40 +0700

    Update caculator.py

--- caculator.py
+++ caculator.py
@@ -1,8 +1,13 @@
-def uppercase_name(name):
-    return name.upper()
+def add(a, b):
+    return a + b
 
-def lowercase_name(name):
-    return name.lower()
+def subtract(a, b):
+    return a - b
 
-def capitalize_name(name):
-    return name.title()
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Không thể chia cho 0"
+    return a / b
